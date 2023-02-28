04affd1249c6d4a598fb05c99af6f532fa2eae9f
David Reti <development@davidreti.net>
2023-02-27 22:50:22 -0500

    Add header and footer

--- src/pokeme/components/footer/Footer.js
+++ src/pokeme/components/footer/Footer.js
@@ -1,6 +1,12 @@
-const Footer = () =>
-    <footer>
+import './Footer.css'
 
+const Footer = () =>
+    <footer className='footer'>
+        <p className='footer__text'>
+            This site was developed by David Reti from <a href='https://davidreti.net'>davidreti.net</a>.
+            <br/>
+            Credit to Three-shots from Pexels for the background image. Thanks!
+        </p>
     </footer>
 
 export default Footer;
--- src/pokeme/components/header/Header.js
+++ src/pokeme/components/header/Header.js
@@ -1,6 +1,14 @@
-const Header = () =>
-    <header>
+import './Header.css'
+import Logo from '../../assets/Logo.png'
+import { Link } from 'react-router-dom';
 
+const Header = () =>
+    <header className='header'>
+        <div className='header__background'></div>
+        <Link to={'/'} className='header__link'>
+            <img className='header__link__logo' src={Logo} alt=''></img>
+            <h1 className='header__link__title'>PokéMe</h1>
+        </Link>
     </header>
 
 export default Header;
