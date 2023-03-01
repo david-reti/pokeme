76a2b05a3251c23d43c450a995d7f6d88e2ec7d2
David Reti <development@davidreti.net>
2023-03-01 11:05:32 -0500

    Add more detail to routing error page

--- src/pokeme/pages/error/Error.js
+++ src/pokeme/pages/error/Error.js
@@ -1,10 +1,17 @@
 import { useRouteError } from "react-router-dom";
+import { ActionLink } from "../../components/action/Action";
 
 export default function Error () {
     const routeError = useRouteError();
 
-    return <>
-        <strong>Unfortunately, a routing error has occured:</strong>
-        <p>{routeError.statusText || routeError.message}</p>
-    </>
+    return <div className="container">
+        <h2 className="title title--small">Unfortunately, a routing error has occured</h2>
+        <p className="description">{routeError.statusText || routeError.message}</p>
+        <p className="description">
+            This is likely because you are trying to access a URL which doesn't exist, or a required component failed to load. 
+        </p>
+        <div className="row">
+            <ActionLink to={'/'}>Back to Root</ActionLink>
+        </div>
+    </div>
 }
